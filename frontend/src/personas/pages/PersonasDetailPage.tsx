3c5115b989efb20b53410adb4f2fdef84a6ce341
mauro <torres.nicolas.mauro@gmail.com>
2023-07-09 14:28:03 -0300

    agregando react-hook-form

--- frontend/src/personas/pages/PersonasDetailPage.tsx
+++ frontend/src/personas/pages/PersonasDetailPage.tsx
@@ -10,6 +10,8 @@ import {
   IonTitle,
 } from "@ionic/react";
 import React, { useState } from "react";
+import { Controller, SubmitHandler, useForm } from "react-hook-form";
+
 import PersonaDto from "../types/PersonaDto";
 import { TipoDocumentoEnumDto } from "../types/TipoDocumentoTypes";
 import TipoDocumentoIonSelect from "../components/TipoDocumentoIonSelect";
@@ -23,86 +25,135 @@ const initData: PersonaDto = {
 };
 
 const PersonaDetailPage: React.FC = () => {
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    defaultValues: initData,
+  });
+  const onSubmit: SubmitHandler<PersonaDto> = (data) => console.log(data);
+
   const [persona, setPersona] = useState<PersonaDto>(initData);
   return (
     <IonPage>
       <IonContent>
-        <IonGrid>
-          <IonRow>
-            <IonCol>
-              <IonItem>
-                <IonTitle>Personas</IonTitle>
-              </IonItem>
-            </IonCol>
-          </IonRow>
-          <IonRow>
-            <IonCol size="6">
-              <IonInput
-                fill="outline"
-                labelPlacement="floating"
-                label="Nombre"
-                type="text"
-                value={initData.nombre}
-                onIonInput={(e) => {
-                  setPersona({ ...persona, nombre: e.detail.value! });
-                }}
-              ></IonInput>
-            </IonCol>
-            <IonCol size="6">
-              <IonInput
-                fill="outline"
-                labelPlacement="floating"
-                label="Apellido"
-                type="text"
-                value={initData.apellido}
-                onIonInput={(e) => {
-                  setPersona({ ...persona, apellido: e.detail.value! });
-                }}
-              ></IonInput>
-            </IonCol>
-          </IonRow>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <IonGrid>
+            <IonRow>
+              <IonCol>
+                <IonItem>
+                  <IonTitle>Personas</IonTitle>
+                </IonItem>
+              </IonCol>
+            </IonRow>
+            <IonRow>
+              <IonCol size="6">
+                <Controller
+                  name="nombre"
+                  control={control}
+                  render={() => (
+                    <IonInput
+                      fill="outline"
+                      labelPlacement="floating"
+                      label="Nombre"
+                      type="text"
+                      value={persona.nombre}
+                      onIonInput={(e) => {
+                        setPersona({ ...persona, nombre: e.detail.value! });
+                      }}
+                    />
+                  )}
+                />
+              </IonCol>
+              <IonCol size="6">
+                <Controller
+                  name="apellido"
+                  control={control}
+                  render={() => (
+                    <IonInput
+                      fill="outline"
+                      labelPlacement="floating"
+                      label="Apellido"
+                      type="text"
+                      value={initData.apellido}
+                      onIonInput={(e) => {
+                        setPersona({ ...persona, apellido: e.detail.value! });
+                      }}
+                    />
+                  )}
+                />
+              </IonCol>
+            </IonRow>
 
-          <IonRow>
-            <IonCol size="6">
-              <TipoDocumentoIonSelect
-                isFiltro={false}
-                value={persona.tipoDocumento}
-                setValue={(e) => setPersona({ ...persona, tipoDocumento: e })}
-              ></TipoDocumentoIonSelect>
-            </IonCol>
-            <IonCol size="6">
-              <IonInput
-                fill="outline"
-                labelPlacement="floating"
-                label="Numero Documento"
-                type="text"
-                value={initData.numeroDocumento}
-                onIonInput={(e) => {
-                  setPersona({ ...persona, numeroDocumento: e.detail.value! });
-                }}
-              ></IonInput>
-            </IonCol>
-          </IonRow>
-          <IonRow>
-            <IonCol size="6">
-              <IonInput
-                fill="outline"
-                labelPlacement="floating"
-                label="Fecha Nacimiento"
-                type="date"
-                value={initData.numeroDocumento}
-                onIonInput={(e) => {
-                  setPersona({ ...persona, numeroDocumento: e.detail.value! });
-                }}
-              ></IonInput>
-            </IonCol>
-            <IonCol offset="5" className="ion-padding-vertical">
-              <IonButton fill="solid" onClick={() => {}}>
-                Crear
-              </IonButton>
-            </IonCol>
-          </IonRow>
-        </IonGrid>
+            <IonRow>
+              <IonCol size="6">
+                <Controller
+                  name="tipoDocumento"
+                  control={control}
+                  render={() => (
+                    <TipoDocumentoIonSelect
+                      isFiltro={false}
+                      value={persona.tipoDocumento}
+                      setValue={(e) =>
+                        setPersona({ ...persona, tipoDocumento: e })
+                      }
+                    />
+                  )}
+                />
+              </IonCol>
+              <IonCol size="6">
+                <Controller
+                  name="numeroDocumento"
+                  control={control}
+                  render={() => (
+                    <IonInput
+                      fill="outline"
+                      labelPlacement="floating"
+                      label="Numero Documento"
+                      type="text"
+                      value={initData.numeroDocumento}
+                      onIonInput={(e) => {
+                        setPersona({
+                          ...persona,
+                          numeroDocumento: e.detail.value!,
+                        });
+                      }}
+                    />
+                  )}
+                />
+              </IonCol>
+            </IonRow>
+            <IonRow>
+              <IonCol size="6">
+                <Controller
+                  name="numeroDocumento"
+                  control={control}
+                  render={() => (
+                    <IonInput
+                      fill="outline"
+                      labelPlacement="floating"
+                      label="Fecha Nacimiento"
+                      type="date"
+                      value={initData.numeroDocumento}
+                      onIonInput={(e) => {
+                        setPersona({
+                          ...persona,
+                          numeroDocumento: e.detail.value!,
+                        });
+                      }}
+                    />
+                  )}
+                />
+              </IonCol>
+              <IonCol offset="5" className="ion-padding-vertical">
+                <IonButton type="submit" fill="solid">
+                  Crear
+                </IonButton>
+              </IonCol>
+            </IonRow>
+          </IonGrid>
+        </form>
       </IonContent>
     </IonPage>
   );
